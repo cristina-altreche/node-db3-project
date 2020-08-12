0964e8886543b106b53a88d5945fbe77896f30dc
Cristina Altreche <c.altreche@gmail.com>
2020-08-12 18:15:50 -0400

    findBySteps complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,25 @@
 const dbConfig = require("../data/db-config");
+const mappers = require("./mappers");
+
 
 module.exports ={
     find,
+    findById,
+    findSteps
 }
 
 function find() {
     return dbConfig("schemes")
 }
+
+function findById(id) {
+    return dbConfig("schemes")
+    .where({id})
+    .first()
+}
+
+function findSteps(id) {
+    return dbConfig("steps")
+      .where("id", id)
+      .then(steps => steps.map(step => mappers.stepsToBody(step)));
+  }
