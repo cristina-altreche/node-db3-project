5d7d288670e311ce126c7e1e53b2f8909ae62104
Cristina Altreche <c.altreche@gmail.com>
2020-08-12 16:58:17 -0400

    Two queries complete

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+
+server.get("/", (req, res) => {
+  res.send({ message: "Hello from the API" });
+});
+
+server.use("/api/schemes", SchemeRouter);
 
 module.exports = server;
