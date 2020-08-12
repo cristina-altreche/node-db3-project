2a3b9537cdd83974d651556ee6ff8d5d1b7af065
Cristina Altreche <c.altreche@gmail.com>
2020-08-12 19:08:20 -0400

    MVP met

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,25 +1,44 @@
 const dbConfig = require("../data/db-config");
 const mappers = require("./mappers");
 
-
-module.exports ={
-    find,
-    findById,
-    findSteps
-}
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  remove,
+  update,
+};
 
 function find() {
-    return dbConfig("schemes")
+  return dbConfig("schemes");
 }
 
 function findById(id) {
-    return dbConfig("schemes")
-    .where({id})
-    .first()
+  return dbConfig("schemes").where({ id }).first();
 }
 
 function findSteps(id) {
-    return dbConfig("steps")
-      .where("id", id)
-      .then(steps => steps.map(step => mappers.stepsToBody(step)));
-  }
+  return dbConfig("steps")
+    .where("id", id)
+    .then((steps) => steps.map((step) => mappers.stepsToBody(step)));
+}
+
+function add(scheme) {
+  return dbConfig("schemes")
+    .insert(scheme, "id")
+    .then(([id]) => findById(id));
+}
+
+function update(changes, id) {
+  return dbConfig("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return dbConfig("schemes").where("id", id).del();
+}
